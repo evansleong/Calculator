f0eb71513b2358d4018d47f7b15d87a66152394d
evansleong <141153962+evansleong@users.noreply.github.com>
2023-12-11 16:18:28 +0800

    Update rockpaperscissor.py

--- rockpaperscissor.py
+++ rockpaperscissor.py
@@ -2,26 +2,33 @@ import random
 
 
 def get_user_choice():
-    user_choice = input("Enter Rock, Paper or Scissor: ")
+    user_choice = input("Enter R(ROCK), P(PAPER) or S(SCISSOR): ").lower()
+    while user_choice != "r" and user_choice != "p" and user_choice != "s":
+        user_choice = input("PLEASE RE-ENTER: ").lower()
     return user_choice
 
 
 def generate_ai_choice():
-    return random.choice(['Rock', 'Paper', 'Scissor'])
+    return random.choice(['r', 'p', 's'])
 
 
 def determine_winner(user_choice, ai_choice):
     user_score = 0
-    print(ai_choice)
+    if ai_choice == 'r':
+        print("AI CHOSE ROCK")
+    elif ai_choice == 'p':
+        print("AI CHOSE PAPER")
+    elif ai_choice == 's':
+        print("AI CHOSE SCISSOR")
     if user_choice == ai_choice:
-        print("draw")
-    elif ((user_choice == "Rock" and ai_choice == "Scissor")
-          or (user_choice == "Paper" and ai_choice == "Rock")
-          or (user_choice == "Scissor" and ai_choice == "Paper")):
-        print("You won")
+        print("DRAW")
+    elif ((user_choice == "r" and ai_choice == "s")
+          or (user_choice == "p" and ai_choice == "r")
+          or (user_choice == "s" and ai_choice == "p")):
+        print("YOU WON")
         user_score += 1
     else:
-        print("ai won")
+        print("AI WON")
 
     print("Your score :", user_score)
 
